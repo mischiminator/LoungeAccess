0acac85ab6e448f0214517ef7cc58ce5ce4b2115
Kariko <38972920+mischiminator@users.noreply.github.com>
2022-11-18 19:13:36 +0100

    added favicon

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!doctype html>
 <html>
+<?php 
+
+require_once('include/polyfill.php');
+require_once('include/config.php');
+?>
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Gigamot Charger Lounge</title>
+  <link rel="icon" type="image/png" href="<?=$config['logofile']?>" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link href="css/folders.css" rel="stylesheet" />
@@ -14,8 +20,6 @@
 <body>
 
   <?php
-  require_once('include/polyfill.php');
-  require_once('include/config.php');
 
   date_default_timezone_set($config['timezone']);
   require_once('include/DB.php');
